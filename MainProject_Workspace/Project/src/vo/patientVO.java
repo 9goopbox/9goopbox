30e47bcfc234722e99ba518a76bc6465234db686
bongsync <bongsync@naver.com>
2015-02-14 17:48:09 +0900

    UI
    UI수정

---
+++ MainProject_Workspace/Project/src/vo/patientVO.java
@@ -0,0 +1,50 @@
+package vo;
+
+public class patientVO {
+	private int id, age;
+	private String name, sex, phone, doctor_id, familydis;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getDoctor_id() {
+		return doctor_id;
+	}
+	public void setDoctor_id(String doctor_id) {
+		this.doctor_id = doctor_id;
+	}
+	public String getFamilydis() {
+		return familydis;
+	}
+	public void setFamilydis(String familydis) {
+		this.familydis = familydis;
+	}
+	
+	
+}
