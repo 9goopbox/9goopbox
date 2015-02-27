87a805ba2735a7fc1f7e7f248172c05dfd227c37
Ch. <ccwpc@hanmail.net>
2015-02-28 01:39:57 +0900

    article json 형식 지정중

--- MainProject_Workspace/Project/WebContent/js/timeline_common.js
+++ MainProject_Workspace/Project/WebContent/js/timeline_common.js
@@ -4,9 +4,35 @@
 
 // tl 상단에 삽입
 function tlInsert(parent_obj) {
-	$(parent_obj).
+	//$(parent_obj).
 }
 
+/**
+ * {
+ * 	"articles" :
+ * 	[
+ * 		"user": {
+ * 			"id":"employee.id",
+ * 			"name" : "emplyoee.name",
+ * 			"date" : "article.date",
+ * 		},
+ * 		"article": {
+ * 			"cont":"cont"
+ * 			"resp":
+ * 		}
+ * 		"media": {
+ * 
+ * 		}
+ *  ],
+ *  "count" : 글갯수 
+ * }
+ */
+
+/**
+ * 
+ * @param artDat
+ * @returns
+ */
 function makeArticle(artDat) {
 	
 }
